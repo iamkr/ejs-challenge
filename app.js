e0d7c910d60eb4f92e6b8f3166c1808cb63d4d6c
iamkr <amalrajendran1@gmail.com>
2023-02-03 14:54:50 +0530

    add various routes with its layouts

--- app.js
+++ app.js
@@ -15,9 +15,24 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/",(req, res) =>{
-  res.render("home")
-})
+app.get("/",function(req, res){
+  res.render("home",{
+    homeStartingContent:homeStartingContent})
+});
+
+app.get("/about",function(req, res){
+  res.render("about",{
+    aboutContent:aboutContent})
+});
+
+app.get("/contact",function(req, res){
+  res.render("contact",{
+    contactContent:contactContent})
+});
+
+app.get("/compose",function(req, res){
+  res.render("compose")
+});
 
 
 
